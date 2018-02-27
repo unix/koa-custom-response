d2fc261fa2bd5029c271dc0c672b30d1b1f267c6
WittBulter <nanazuimeng123@gmail.com>
2018-02-27 18:13:26 +0800

    ci(pkg): fix nyc error

--- test/utils.nyc.ts
+++ test/utils.nyc.ts
@@ -3,21 +3,23 @@ import * as server from '../examples/app'
 import * as chai from 'chai'
 import * as chaiHttp from 'chai-http'
 import { ChaiHttp } from 'chai-http'
+import { Server } from "http"
 
 chai.use(chaiHttp)
 
 let timer: Timer
-const closeServer = (): void => {
+const closeServer = (app: Server): void => {
   timer && clearTimeout(timer)
   timer = global.setTimeout(() => {
-    process.exit(0)
+    app.close()
     clearTimeout(timer)
-  }, 3000)
+  }, 100)
 }
 
 export const app = async(): Promise<ChaiHttp.Agent> => {
-  closeServer()
-  return chai.request(await server)
+  const app: Server = await server
+  closeServer(app)
+  return chai.request(app)
 }
 
 export const paramsToString = (params: { [key: string]: any }): string => {
